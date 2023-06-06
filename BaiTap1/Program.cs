0cf74175067eaebb2dd3d90d08b2fdffb1b83ff7
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 00:18:38 +0700

    DungNguyen

---
+++ BaiTap1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap1
+{
+    internal class Program
+    {
+        public static int soLuongKhoaLuan(List<sinhVien> svKhoaLuan, List<sinhVien> sinhViens)
+        {
+            int result = 0;
+            foreach (sinhVien s in sinhViens)
+            {
+                if (s.diemLapTrinh >= 5.0 && s.diemCSDL >= 5 && s.diemWeb >= 5 && (s.diemLapTrinh + s.diemCSDL + s.diemWeb)/3 >= 8)
+                {
+                    result++;
+                    svKhoaLuan.Add(s);
+                }
+            }
+            return result;
+        }
+
+        public static int soLuongChuyenDe(List<sinhVien> svChuyenDe, List<sinhVien> sinhViens)
+        {
+            int result = 0;
+            foreach (sinhVien s in sinhViens)
+            {
+                if (s.diemLapTrinh >= 5.0 && s.diemCSDL >= 5 && s.diemWeb >= 5 && (s.diemLapTrinh + s.diemCSDL + s.diemWeb)/3 < 8)
+                {
+                    result++;
+                    svChuyenDe.Add(s);
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int n;
+            do
+            {
+                Console.Write("Nhap so luong sinh vien: n = ");
+                n = int.Parse(Console.ReadLine().Trim());
+            } while (n <= 0);
+
+            List<sinhVien> sinhViens = new List<sinhVien>();
+
+            // Nhap ds sinh vien
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nhap sinh vien thu " + (i + 1));
+
+                sinhVien sv = new sinhVien();
+                sv.nhapSinhVien();
+                sinhViens.Add(sv);
+
+                Console.WriteLine("------------------------------");
+            }
+
+
+            List<sinhVien> svKhoaLuan = new List<sinhVien>();   // Lưu các sv làm khóa luận tốt nghiệp
+
+            Console.WriteLine("So luong sinh vien duoc lam khoa luan tot nghiep la: " + soLuongKhoaLuan(svKhoaLuan, sinhViens));
+            Console.WriteLine("Danh sach sinh vien duoc lam khoa luan tot nghiep:");
+            Console.Write("\n{0,20}" + "|" + "{1,9}" + "|" + "{2,15}" + "|" + "{3,15}" + "|" + "{4,15}",
+                "Ho ten", "Ngay sinh", "Diem lap trinh", "Diem CSDL", "Diem Web");
+            foreach (sinhVien s in svKhoaLuan)
+            {
+                s.Display();
+            }
+
+
+            List<sinhVien> svChuyenDe = new List<sinhVien>();   // Lưu các sv làm chuyên đề tốt nghiệp
+            Console.WriteLine("\nSo luong sinh vien duoc lam chuyen de tot nghiep la: " + soLuongChuyenDe(svChuyenDe, sinhViens));
+            Console.WriteLine("Danh sach sinh vien duoc lam chuyen de tot nghiep:");
+            Console.Write("\n{0,-20}" + "|" + "{1,10}" + "|" + "{2,15}" + "|" + "{3,15}" + "|" + "{4,15}",
+                "Ho ten", "Ngay sinh", "Diem lap trinh", "Diem CSDL", "Diem Web");
+            foreach (sinhVien s in svChuyenDe)
+            {
+                s.Display();
+            }
+        }
+    }
+}
---
+++ BaiTap1/sinhVien.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap1
+{
+    internal class sinhVien
+    {
+        public string hoTen;
+        public DateTime dateOfBirth;
+        public double diemLapTrinh;
+        public double diemCSDL;
+        public double diemWeb;
+
+        public sinhVien() { }
+
+        public sinhVien(string hoTen, DateTime dob, double diemLT, double diemCSDL, double diemWeb)
+        {
+            this.hoTen = hoTen;
+            this.dateOfBirth = dob;
+            this.diemLapTrinh = diemLT;
+            this.diemCSDL = diemCSDL;
+            this.diemWeb = diemWeb;
+        }
+
+        public void nhapSinhVien()
+        {
+            Console.Write("Nhap ho ten sinh vien: ");
+            hoTen = Console.ReadLine().Trim();
+
+            // Nhập ngày sinh dạng dd/mm/yyyy
+            Console.Write("\nNhap ngay sinh (dd/mm/yyyy) : ");
+            string input = (Console.ReadLine()).Trim();
+            string[] arrListStr = input.Split('/', ' ', '.', '-', '_');
+            int ngay = int.Parse(arrListStr[0]);
+            int thang = int.Parse(arrListStr[1]);
+            int nam = int.Parse(arrListStr[2]);
+            dateOfBirth = new DateTime(nam, thang, ngay);
+
+            Console.Write("\nNhap diem mon lap trinh: ");
+            diemLapTrinh = Convert.ToDouble(Console.ReadLine().Trim());
+            Console.Write("\nNhap diem mon co so du lieu: ");
+            diemCSDL = Convert.ToDouble(Console.ReadLine().Trim());
+            Console.Write("\nNhap diem mon thiet ke web: ");
+            diemWeb = Convert.ToDouble(Console.ReadLine().Trim());
+        }
+
+        public void Display()
+        {
+            Console.Write("\n{0,20}" + "|" + "{1,9}" + "|" + "{2,15}" + "|" + "{3,15}" + "|" + "{4,15}",
+                hoTen, dateOfBirth.ToString("dd/MM/yyyy"), diemLapTrinh, diemCSDL, diemWeb);
+        }
+    }
+}
